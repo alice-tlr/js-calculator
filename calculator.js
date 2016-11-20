602fa371ae2bcdaa38e2d43b24d76c5a941db1bf
Alice Rosier <alicetlrosier@gmail.com>
2016-11-20 15:40:22 +1300

    Removed unecessary ELSE from JS; tweaked some stuff in HTML

--- calculator.js
+++ calculator.js
@@ -3,34 +3,26 @@
 var stream = '';
 
 function calculate(){
-  var result = eval(stream);//the result = the evaluation of whatever the stream variable is (this var relys on the outcome of another vars value, in thecase var streams value)
-
-  stream = result;//here we change value of stream to the value of result.
-  document.getElementById('display').innerHTML = result;//where we are putting the result within the HTML
-  console.log(result);//print the result
+  var result = eval(stream);
+  stream = result;
+  document.getElementById('display').innerHTML = result;
+  console.log(result);
 }
 
 function c(){
-  stream = "";//make stream equal to nothing
-  document.getElementById('display').innerHTML = stream;//put steam here in HTML
-
+  stream = "";
+  document.getElementById('display').innerHTML = stream;
 }
 
 function number(a){
-
-    if(stream === ''){//if steam is = to nothing
-      stream = '' + a;//then steam = whatever 'a' is (function number(a)) - we're adding '' empty string at the start to make sure its stored as a string not a number. if it was a number, when we do += it will actually calulate rather than concatinate. e.g  1+1 = 2 rather than 1 + 1 = 11
+      if(stream === ''){
+      stream = '' + a;
     }
-    else if(isNumber(a)){//else/if if number is in the last position, print to console
+    else if(isNumber(a)){
       console.log(a);
-      stream += a;//if its the last number, stream = stream plus a 0 on the end
+      stream += a;
     }
 
-    else{
-      stream += ' ' + a + ' ';//if we pass in a symbol, add it to the stream
-      //stream += 'something'; is the same as
-      //stream = stream + 'something';
-    }
     document.getElementById('display').innerHTML = stream;//display it in this bit of html
 }
 
